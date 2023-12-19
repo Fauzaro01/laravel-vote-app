12e58a2928f623607d60b4339b73c2a52c4b1279
Fauzaro01 <muhamadfauzan4750@gmail.com>
2023-12-19 08:06:55 +0700

    + Add Root Controller

--- app/Http/Controllers/RootController.php
+++ app/Http/Controllers/RootController.php
@@ -2,15 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class RootController extends Controller
 {
-    public function root() {
+    public function root()
+    {
         return view('index');
     }
 
-    public function showvote () {
-        return "200";
+    public function showvote($id)
+    {
+        // Lakukan sesuatu dengan nilai $id, misalnya ambil data dari database
+        $data = Post::find($id);
+        $vote = $data->votes;
+
+        // Tampilkan data atau lakukan tindakan lain
+        return view('posts.postingan', ['data' => $data, 'votes' => $vote]);
     }
 }
