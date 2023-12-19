ed4611701c7d8ed4746986ffe5fe4869ee186ddf
Fauzaro01 <muhamadfauzan4750@gmail.com>
2023-12-19 08:07:37 +0700

    + Add Vote request POST

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use App\Models\PostVote;
 use App\Models\Vote;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rule;
 use illuminate\Support\Str;
 use Illuminate\Http\Request;
 
@@ -15,39 +17,44 @@ class PostController extends Controller
         $this->middleware('auth');
     }
 
-    public function test() {
-        return "Hello World 200!";
+    public function test()
+    {   
+        $votePosts = Post::find('');
+        return view('test.index');
     }
 
-    public function index() {
+    public function index()
+    {
         $votePosts = Post::all();
         return view('posts.index', compact('votePosts'));
     }
 
-    public function showform() {
+    public function showform()
+    {
         $this->middleware('role:admin');
         return view('posts.formvote');
     }
 
-    public function store(Request $request) {
+    public function store(Request $request)
+    {
         $this->middleware('role:admin');
-        
+
         $request->validate([
             'title' => "required|min:4",
             'content' => "required|min:4",
             'options' => "required|array|max:5"
         ]);
 
-    
+
         $postingan = Post::create([
             'id' => Str::random(13),
             'title' => $request->title,
-            'content'=> $request->content,
-            'user_id'=> Auth::user()->id
-        ]); 
+            'content' => $request->content,
+            'user_id' => Auth::user()->id
+        ]);
 
         $dataku = [];
-        foreach($request->options as $value) {
+        foreach ($request->options as $value) {
             $dataku[] = [
                 'id' => Str::random(13),
                 'name' => $value,
@@ -60,7 +67,35 @@ class PostController extends Controller
         $postingan->votes()->createMany($dataku);
 
         return response()->json(["msg" => "Berhasil Bro :D"]);
+    }
 
+    public function votepost(Request $request)
+    {
+        // Validasi input
+        $request->validate([
+            'post_id' => 'required|exists:posts,id',
+            'vote_id' => [
+                'required',
+                'exists:votes,id',
+                Rule::unique('post_votes', 'vote_id')->where(function ($query) use ($request) {
+                    return $query->where('user_id', auth()->id())
+                        ->where('post_id', $request->post_id);
+                }),
+            ],
+        ]);
+
+        // Logika pemrosesan suara
+        $postVote = new PostVote([
+            'user_id' => auth()->id(),
+            'vote_id' => $request->vote_id,
+        ]);
+        $postVote->save();
+
+        // Increment jumlah suara pada kandidat terkait
+        $vote = Vote::find($request->vote_id);
+        $vote->increment('value');
+
+        return redirect()->back()->with('success', 'Suara Anda telah tercatat!');
     }
 
 }
