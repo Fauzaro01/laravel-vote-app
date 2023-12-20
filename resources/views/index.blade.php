890140f22e6d00d6c7a05b20f08c4a3c8bc62bd4
Fauzaro01 <muhamadfauzan4750@gmail.com>
2023-12-20 21:31:30 +0700

    + Tombol doang..

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -13,8 +13,7 @@ Halaman Utama
       Kami adalah platform inovatif yang dirancang untuk memberikan suara kepada masyarakat dalam berbagai keputusan dan pemilihan. <br>Di Stellar Vote, kami memahami pentingnya pendapat setiap individu, dan itulah mengapa kami menghadirkan sistem voting online yang mudah digunakan dan dapat diakses oleh semua.
     </p>
       <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-        <a href="{{route('login')}}" class="btn btn-dark btn-lg px-4 gap-3">Log in</a>
-        <a href="{{route('register')}}" class="btn btn-dark btn-lg px-4">Register</a>
+        <a href="{{route('login')}}" class="btn btn-dark btn-lg px-4 gap-3">Bergabung</a>
       </div>
     </div>
   </div>
