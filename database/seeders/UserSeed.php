bc5d995c5fdc8bb5ced46728d566ad52a12b75b8
Fauzaro01 <muhamadfauzan4750@gmail.com>
2024-01-08 11:26:41 +0700

    + Lupa di hash passwordnya untuk Seed

--- database/seeders/UserSeed.php
+++ database/seeders/UserSeed.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 use App\Models\User;
 
@@ -19,13 +20,13 @@ class UserSeed extends Seeder
                 'id' => Str::random(13),
                 'username' => "fauzaro01",
                 'email' => 'muhamadfauzan4750@gmail.com',
-                'password' => 'admin123',
+                'password' => Hash::make('admin123'),
                 'role' => 'admin'
             ],[
                 'id' => Str::random(13),
                 'username' => "zarory",
                 'email' => 'zarory01@gmail.com',
-                'password' => 'member123',
+                'password' => Hash::make('member123'),
                 'role' => 'member'
             ]
         ];
